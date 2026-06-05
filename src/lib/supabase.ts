01992a0f08c838b1f731662ff568cbccc22dc0bf
earnwisegr-lgtm <earnwisegr@gmail.com>
2026-06-05 14:14:39 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qlmmbudwpttjtssIrvqm.supabase.co';
+const supabaseUrl = 'https://qlmmbudwpttjtsslrvqm.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFsbW1idWR3cHR0anRzc2xydnFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxOTI3ODEsImV4cCI6MjA5Mzc2ODc4MX0.gAb_osLSuySNRvizE-lCQf6t527Hd3UWtPVl-kCU6xg';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
